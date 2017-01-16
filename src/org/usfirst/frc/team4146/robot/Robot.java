603343ab0ca5802f125ec1ecc11118c7df998523
GowanR <gowan.rowland@gmail.com>
2017-01-16 11:22:57 -0700

    Project Up

---
+++ src/org/usfirst/frc/team4146/robot/Robot.java
@@ -0,0 +1,36 @@
+
+package org.usfirst.frc.team4146.robot;
+
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+
+
+public class Robot extends SampleRobot{
+	
+    public Robot() {
+    	
+    }
+    
+    public void robotInit() {
+        
+    }
+	
+    public void autonomous() {
+    	
+    }
+    
+    public void operatorControl() {
+    	
+    }
+
+    public void test() {
+    	
+    }
+}
