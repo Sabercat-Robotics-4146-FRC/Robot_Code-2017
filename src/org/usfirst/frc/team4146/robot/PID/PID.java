076f8207902ce4669e4977380fb08cd29609c2ca
GowanR <gowan.rowland@gmail.com>
2017-01-16 16:44:32 -0700

    Initial commit for Jacob_Branch so far writen by gowan.

---
+++ src/org/usfirst/frc/team4146/robot/PID/PID.java
@@ -0,0 +1,73 @@
+package org.usfirst.frc.team4146.robot.PID;
+
+/**
+ * General purpose PID loop.
+ * @author Gowan Rowland
+ * @version 9/8/2016
+ */
+public class PID {
+	private double Kp;
+	private double Ki;
+	private double Kd;
+	private double prevError;
+	private double setpoint;
+	//private double integral;
+	private double error;
+	private double derivative;
+	private signal functions;
+	private double output;
+
+	/* Prevent Integral windup with a SizedStack */
+	private SizedStack integral_stack;
+	/* Make derivative filter with SizedStack */
+	private SizedStack derivative_stack;
+
+	private boolean sp_ramp_enabled;
+	PID sp_ramp_pid;
+
+	public PID ( signal functions ){
+		sp_ramp_enabled = false;
+		this.functions = functions;
+		//integral = 0;
+		setpoint = 0;
+		integral_stack = new SizedStack( 10 );
+		derivative_stack = new SizedStack( 3 );
+	}
+	public void set_integral_range( int n ) {
+		integral_stack.resize( n );
+	}
+	public void set_derivative_range( int n ) {
+		derivative_stack.resize( n );
+	}
+	public void set_setpoint( double s ){
+		if ( sp_ramp_enabled ) {
+			sp_ramp_pid.set_setpoint( s );
+		}
+		setpoint = s;
+	}
+	public void set_pid( double p, double i, double d ) {
+		this.Kp = p;
+		this.Ki = i;
+		this.Kd = d;
+	}
+	public void set_sp_ramp( PID sp_pid ) {
+		sp_ramp_enabled = true;
+		sp_ramp_pid = sp_pid;
+	}
+	public void update( double dt ){
+		if ( sp_ramp_enabled ) {
+			sp_ramp_pid.update( dt );
+			setpoint = sp_ramp_pid.get();
+		}
+		error = setpoint - functions.getValue();
+		integral_stack.push( ( Ki * error * dt ) );
+
+		derivative = ( prevError - error ) / dt;
+		derivative_stack.push( derivative );
+		prevError = error;
+		output = ( Kp * error ) + ( integral_stack.sum() ) + ( Kd * derivative_stack.mean() ); // Note: Should we use mean derivative filter?
+	}
+	public double get() {
+		return output;
+	}
+}
--- src/org/usfirst/frc/team4146/robot/Robot.java
+++ src/org/usfirst/frc/team4146/robot/Robot.java
@@ -4,22 +4,56 @@ package org.usfirst.frc.team4146.robot;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-
+import org.usfirst.frc.team4146.robot.PID.*;
 
 
 public class Robot extends SampleRobot{
+	Controller drive_controller;
+	Talon front_left;
+	Talon rear_left;
+	Talon front_right;
+	Talon rear_right;
+	RobotDrive drive;
 	
+	
+	AHRS gyro;
+	PID heading_pid;
     public Robot() {
+    	drive_controller = new Controller( 0 );
     	
+    	front_left  = new Talon( 5 );
+		rear_left   = new Talon( 6 );
+		front_right = new Talon( 7 );
+		rear_right  = new Talon( 0 );
+		
+		front_left.setSafetyEnabled(false);
+		rear_left.setSafetyEnabled(false);
+		front_right.setSafetyEnabled(false);
+		rear_right.setSafetyEnabled(false);
+		
+		
+		gyro = new AHRS( SPI.Port.kMXP );
+		gyro.reset();
+		
+		// Instantiate robot's drive with Talons
+		drive      = new RobotDrive( front_left, rear_left, front_right, rear_right );
+		heading_pid = new PID( new signal() {
+			public double getValue() {
+				return gyro.getAngle();
+			}
+		});
+		heading_pid.set_pid( 1, 0, 0 );
+		heading_pid.set_setpoint( 0 );
     }
     
     public void robotInit() {
-        
+        System.out.println("Hello from robotInit!");
     }
 	
     public void autonomous() {
@@ -27,8 +61,24 @@ public class Robot extends SampleRobot{
     }
     
     public void operatorControl() {
-    	
-    }
+    	//long last_time = 0;
+    	while ( isOperatorControl() && isEnabled() ) {
+    		//last_time = System.nanoTime();
+			// Reset the gyro with the A button.
+//    		if ( drive_controller.get_a_button() ) {
+//    			gyro.reset();
+//    		}
+//    		System.out.println( gyro.getAngle() );
+    		// Initiate goto angle with left trigger.
+//    		if ( drive_controller.get_left_trigger() ) {
+//    			System.out.println( heading_pid.get() );
+//    			drive.arcadeDrive( 0, -1 * heading_pid.get() );
+//    		} else {
+    			drive.arcadeDrive( -1 * drive_controller.get_left_y_axis(), -1 * drive_controller.get_right_x_axis() );
+//    		}
+    		//heading_pid.update( (double) ( last_time - System.nanoTime() ) * 1e-9 );
+    	}
+    }// end operatorControl
 
     public void test() {
     	
