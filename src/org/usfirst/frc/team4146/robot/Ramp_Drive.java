3c7e557c47be70b95b4b96505c00c877330f961e
GowanR <gowan.rowland@gmail.com>
2017-01-19 16:25:38 -0700

    Updated Ramp Drive code to almost finalized (maybe) version.

--- src/org/usfirst/frc/team4146/robot/Ramp_Drive.java
+++ src/org/usfirst/frc/team4146/robot/Ramp_Drive.java
@@ -5,19 +5,23 @@ import edu.wpi.first.wpilibj.Timer;
 public class Ramp_Drive {
 	
 	public double speed = 0.0;
-	public double accelerate_rate = 0.55;
+	public double accelerate_rate = 0.65;
 	public double decelerate_rate = 1.0;
 	public double dt = 1e-3;
 	public long system_time = System.nanoTime();
-	private double ctrl_deadband = 0.1;
-	private double mech_deadband = 0.4;
-	
+	private double ctrl_deadband = 0.15;
+	private double mech_deadband = 0.3;
+	private double mSlope;
+	private double bIntercept;
+	private double targetSpeed;
 	private Controller drive_controller;
 	private RobotDrive drive;
 	
 	Ramp_Drive( Controller dc, RobotDrive d ){
 		drive_controller = dc;
 		drive = d;
+		mSlope = ((1.0 - mech_deadband) / (1.0 - ctrl_deadband));
+		bIntercept = ((-ctrl_deadband * mSlope) + mech_deadband);
 		
 	}
 	
@@ -26,43 +30,47 @@ public class Ramp_Drive {
 		system_time = System.nanoTime();
 		double left_y = -drive_controller.get_left_y_axis();
 		
-		check_speed(left_y);
+		left_y = check_speed(left_y);
 		drive.arcadeDrive( speed, -1 * drive_controller.get_right_x_axis() );
-		System.out.println( speed );
-		//Timer.delay(0.005);	
+		System.out.printf("% 5.2f -- % 5.2f -- % 5.2f \n", left_y, targetSpeed, speed );
+		Timer.delay(0.005);	
 	}
-	public void check_speed(double left_y) {
+	public double check_speed(double left_y) {
 		left_y = controller_deadband(left_y);
-		mechanical_deadband(left_y);    //FINISH THIS SHITLORD
-		quick_stop(left_y);
-		
-		if ( (left_y > speed) && (left_y > 0)){ //Traveling forward but speed is not fast enough
+		mechanical_deadband(left_y);
+		targetSpeed = find_target_speed(left_y);
+		//quick_stop(left_y);
+		if(/*(targetSpeed == 0.0) &&*/ ((speed > -mech_deadband) && (speed < mech_deadband))){
+			speed = 0.0;
+		}
+		else if ( ( targetSpeed > speed) && ( targetSpeed > 0)){ //Traveling forward but speed is not fast enough
 			speed +=  accelerate_rate * dt;
 		}
-		else if((left_y < speed) && (left_y < 0)){ //Traveling backwards but speed is not fast enough
+		else if(( targetSpeed < speed) && ( targetSpeed < 0)){ //Traveling backwards but speed is not fast enough
 			speed -= accelerate_rate * dt;
 		}
-		else if((left_y < speed) && (left_y >= 0)){//Traveling forwards but speed is too fast
+		else if(( targetSpeed < speed) && ( targetSpeed >= 0)){//Traveling forwards but speed is too fast
 			speed -= decelerate_rate * dt;
 		}
-		else if( (left_y > speed) && (left_y <= 0)){//Traveling backwards but speed is too fast
+		else if( ( targetSpeed > speed) && ( targetSpeed <= 0)){//Traveling backwards but speed is too fast
 			speed +=  decelerate_rate * dt;
 		}
 		
+		return left_y;
 	}
-	private void quick_stop(double left_y){	//if speed is opposite sign of left y then instant stop
+	private void quick_stop(double left_y){	//if speed is opposite sign of left y then instant stop //Probably Useless
 		if(((left_y > 0) && (speed < 0)) || ((left_y < 0) && (speed > 0))){
 			speed = 0.0;
 		}
 	}
-	private double controller_deadband(double left_y){
-		if((left_y < ctrl_deadband) && (left_y > -ctrl_deadband))
+	private double controller_deadband(double l_y){
+		if((l_y < ctrl_deadband) && (l_y > -ctrl_deadband))
 		{
-			return 0.0;
+			return (double)(0.0);
 		}
 		else
 		{
-			return left_y;
+			return l_y;
 		}
 	}
 	private void mechanical_deadband(double left_y){
@@ -72,6 +80,16 @@ public class Ramp_Drive {
 		else if((left_y < 0) && (speed == 0.0)){
 			speed = -mech_deadband;
 		}
+	}
+	private double find_target_speed( double left_y ){
+		if( left_y == 0.0 ){
+			return 0.0;
+		}else if(left_y > 0.0){
+			return ( ( mSlope * left_y) + bIntercept );
+		}else if(left_y < 0.0){
+			return ( ( mSlope * left_y) - bIntercept );
+		}
+		return 0.0;
 	}
 	
 }
