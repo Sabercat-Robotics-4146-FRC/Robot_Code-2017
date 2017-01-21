504231e7ce1131b877beef84c931be571794008e
keanetevelde <keanetevelde@gmail.com>
2017-01-21 14:54:37 -0700

    Fixed formatting again

--- src/org/usfirst/frc/team4146/robot/Robot.java
+++ src/org/usfirst/frc/team4146/robot/Robot.java
@@ -1,9 +1,5 @@
-
 package org.usfirst.frc.team4146.robot;
-
-
 import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SampleRobot;
@@ -11,8 +7,8 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import org.usfirst.frc.team4146.robot.PID.*;
 
-
-public class Robot extends SampleRobot{
+public class Robot extends SampleRobot {
+	
 	Controller drive_controller;
 	Talon front_left;
 	Talon rear_left;
@@ -20,10 +16,11 @@ public class Robot extends SampleRobot{
 	Talon rear_right;
 	RobotDrive drive;
 	
-	
 	AHRS gyro;
 	PID heading_pid;
+	
     public Robot() {
+    	
     	drive_controller = new Controller( 0 );
     	
     	front_left  = new Talon( 5 );
@@ -35,13 +32,13 @@ public class Robot extends SampleRobot{
 		rear_left.setSafetyEnabled(false);
 		front_right.setSafetyEnabled(false);
 		rear_right.setSafetyEnabled(false);
-		
-		
+				
 		gyro = new AHRS( SPI.Port.kMXP );
 		gyro.reset();
 		
 		// Instantiate robot's drive with Talons
 		drive = new RobotDrive( front_left, rear_left, front_right, rear_right );
+		
 		heading_pid = new PID( new signal() {
 			public double getValue() {
 				return gyro.getAngle();
@@ -60,7 +57,7 @@ public class Robot extends SampleRobot{
     }
     
     public void operatorControl() {
-    	//long last_time = 0;
+    	
     	Ramp_Drive dTrain = new Ramp_Drive( drive_controller, drive );
     	
     	while ( isOperatorControl() && isEnabled() ) {
