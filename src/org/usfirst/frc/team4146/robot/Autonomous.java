0628b6f561319f8b69d0d9b1ceac51622bc25a2c
Gowan Rowland <GowanR@users.noreply.github.com>
2017-02-01 12:20:22 -0700

    Added encoder tracking.

--- src/org/usfirst/frc/team4146/robot/Autonomous.java
+++ src/org/usfirst/frc/team4146/robot/Autonomous.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4146.robot;
 import org.usfirst.frc.team4146.robot.PID.*;
+import edu.wpi.first.wpilibj.Encoder;
 
 /**
 * Base Autonomous class.
@@ -8,16 +9,32 @@ import org.usfirst.frc.team4146.robot.PID.*;
 */
 class Autonomous {
   Robot robot;
+  Encoder right_drive_encoder;
+	Encoder left_drive_encoder;
+  PID heading_pid;
+  PID encoder_pid;
+  static double encoder_conversion_factor = 666.666; // Hell number to be determined.
+  
   public Autonomous ( Robot robot ) {
     this.robot = robot;
+    // Instantiate drive encoders
+		right_drive_encoder = new Encoder( 2, 3, false ); // false so to not reverse direction
+		left_drive_encoder  = new Encoder( 0, 1, true );
     // Make all pids used in autonomous.
+    encoder_pid = new PID( new signal() {
+      public double getValue() {
+        return convert_encoder_to_feet( (double) right_drive_encoder.getRaw() );
+      }
+    });
+    // TODO: tweak encoder pid values.
+    encoder_pid.set_pid( 0.5, 0, 0 );
     // Make Encoder PID
     
     // Make gyro PID
-    PID heading_pid = new PID( new signal () {
-      double getValue() {
+    heading_pid = new PID( new signal() {
+      public double getValue() {
         // TODO: Make this angle absolute in 180 to -180 system to prevent wrapping behavior.
-        return robot.gyro.getAngle();
+        return convert_wrapped_angle( robot.gyro.getAngle() );
       }
     });
     // TODO: tweak heading pid constants
@@ -34,7 +51,23 @@ class Autonomous {
     }
     return a;
   }
+  private double convert_encoder_to_feet( double e ) {
+    return e * encoder_conversion_factor;
+  }
   public void drive_forward( double feet ) {
+    encoder_pid.set_setpoint( feet );
+    heading_pid.set_setpoint( convert_wrapped_angle( robot.gyro.getAngle() ) );
+    double dt = 1e-3;
+    long start_time;
+    right_drive_encoder.reset();
+    while ( Math.abs( encoder_pid.steady_state_error() ) > 0.5 ) { // until the steady state error is lower than half a foot,
+      start_time = System.nanoTime();
+      // corect angle
+      heading_pid.update( dt );
+      encoder_pid.update( dt );
+      robot.arcadeDrive( encoder_pid.get(), heading_pid.get()  );
+      dt = ( start_time - System.nanoTime() ) * 1e-9; // convert nanosec to sec
+    }
     // a while loop that breaks out with PID steady state error threshold, used pid update. 
   }
   public void turn_to ( double degrees ) {
