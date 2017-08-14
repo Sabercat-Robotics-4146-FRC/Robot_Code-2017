8866d95711bc36a8e5f906439547c2b47eb36268
GowanR <gowan.rowland@gmail.com>
2017-08-14 16:53:49 -0700

    Updated climer values

--- src/org/usfirst/frc/team4146/robot/Climber.java
+++ src/org/usfirst/frc/team4146/robot/Climber.java
@@ -9,17 +9,17 @@ public class Climber {
 	public void update() {
 		
 		if (RobotMap.lifterController.getButtonA()) {
-			RobotMap.climberA.set(1.0);
-			RobotMap.climberB.set(1.0);
+			RobotMap.climberA.set(-0.2);
+			RobotMap.climberB.set(-0.2);
 		} else if (RobotMap.lifterController.getButtonB()) {
-			RobotMap.climberA.set(0.2);
-			RobotMap.climberB.set(0.2);
+			RobotMap.climberA.set(0.1);
+			RobotMap.climberB.set(0.1);
 		} else if (RobotMap.lifterController.getButtonX()) {
-			RobotMap.climberA.set(0.7);
-			RobotMap.climberB.set(0.7);
+			RobotMap.climberA.set(0.35);
+			RobotMap.climberB.set(0.35);
 		} else if (RobotMap.lifterController.getButtonY()) {
-			RobotMap.climberA.set(-0.7);
-			RobotMap.climberB.set(-0.7);
+			RobotMap.climberA.set(0.5);
+			RobotMap.climberB.set(0.5);
 		} else {
 			RobotMap.climberA.set(0.0);
 			RobotMap.climberB.set(0.0);
