bfd280e44658c10b8dd8e9a56251d3fdfb10348b
GowanR <gowan.rowland@gmail.com>
2018-01-15 09:26:44 -0700

    Commiting for Ben

--- src/org/usfirst/frc/team4146/robot/Robot.java
+++ src/org/usfirst/frc/team4146/robot/Robot.java
@@ -26,26 +26,28 @@ public class Robot extends SampleRobot {
 		Talon rearLeft = new Talon(1);
 		Talon frontRight = new Talon(2);
 		Talon rearRight = new Talon(3);
+	
 		
 		frontLeft.setSafetyEnabled(false);
 		rearLeft.setSafetyEnabled(false);
 		frontRight.setSafetyEnabled(false);
 		rearRight.setSafetyEnabled(false);
 		
+		
 		//RobotDrive drive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
 		
 		while (isOperatorControl() && isEnabled()) {
 			if (driver.getButtonA()){ // rear right is backward still need to fix!!!
-		//		rearRight.set(0.5);
+			rearRight.set(0.5);
 			frontRight.set(0.5);
-//				rearLeft.set(0.5);
-//				frontLeft.set(0.5);
+			rearLeft.set(0.5);
+			frontLeft.set(0.5);
 			} else {
-				//rearRight.set(0.0);
+				rearRight.set(0.0);
 			//	rearRight.set(0.0);
-				frontRight.set(0.0);
-//				rearLeft.set(0.0);
-//				frontLeft.set(0.0);
+				frontLeft.set(0.0);
+        		rearLeft.set(0.0);
+				frontLeft.set(0.0);
 			}
 				
 		}
